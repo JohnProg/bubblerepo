499a1c50d0b5506d95df7cdb09d3c7337427fff9
Sumanth <sumanth.van@gmail.com>
2017-11-06 10:50:26 +0530

    The following features have been implemented: 1. From and to text fields on the map. 2. Search using Google map's autocomplete controller 3. Displaying polylines on the map from the source to dest location.

---
+++ bubble1/bubble1/Constants.swift
@@ -0,0 +1,9 @@
+//
+//  Constants.swift
+//  bubble1
+//
+//  Created by Sumanth on 7/17/17.
+//  Copyright © 2017 sumanths. All rights reserved.
+//
+
+import Foundation
---
+++ bubble1/bubble1/HomeViewController.swift
@@ -0,0 +1,9 @@
+//
+//  HomeViewController.swift
+//  bubble1
+//
+//  Created by Sumanth on 7/16/17.
+//  Copyright © 2017 sumanths. All rights reserved.
+//
+
+import Foundation
