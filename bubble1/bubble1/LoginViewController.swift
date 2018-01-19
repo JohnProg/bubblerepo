e948e6db165b4d80180b544b44408cb010e9f3ee
Giridhar Malavali <gmalavali@giridhars-macbook-pro.local>
2018-01-19 02:07:18 -0800

    Added background color and Image to startup screen.

--- bubble1/bubble1/LoginViewController.swift
+++ bubble1/bubble1/LoginViewController.swift
@@ -7,9 +7,7 @@
 //
 
 import UIKit
-import FacebookLogin
-import FacebookCore
-import GoogleMaps
+
 
 class LoginViewController: UIViewController {
 
@@ -17,10 +15,7 @@ class LoginViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        let loginButton = LoginButton(readPermissions: [ .publicProfile ])
-        loginButton.center = view.center
-        
-        view.addSubview(loginButton)
+        self.view.backgroundColor = UIColor(red: 50/255, green: 60/255, blue: 153/255, alpha: 1)
     }
 
     override func didReceiveMemoryWarning() {
@@ -31,77 +26,6 @@ class LoginViewController: UIViewController {
     
     @IBAction func loginClicked(_ sender: Any) {
         (UIApplication.shared.delegate as! AppDelegate).switchToHomeScreen()
-        
-//        (UIApplication.shared.delegate as! AppDelegate).showAuth0Screen()
-        
-        // navigation has to be done in the app delegate
-        
-        
-        /*
-        let loginManager = LoginManager()
-        loginManager.loginBehavior = .native
-        loginManager.logOut()
-        loginManager.logIn([ .publicProfile, .email, .userFriends], viewController: self) {
-            loginResult in
-            switch loginResult {
-            case .failed(let error):
-                print(error)
-                DispatchQueue.main.async {
-//                    self.activityIndicator.stopAnimating()
-                    self.facebookButton.isEnabled = true
-                }
-                
-                break
-            case .cancelled:
-                print("User cancelled login.")
-                DispatchQueue.main.async {
-//                    self.activityIndicator.stopAnimating()
-                    self.facebookButton.isEnabled = true
-                }
-                
-                break
-            case .success:
-                print("Logged in!")
-                if let accessToken = AccessToken.current {
-                    
-                    // User is logged in, use 'accessToken' here.
-                    print("MY ACCESS TOKEN:\(accessToken)")
-                    
-//                    self.fbAccessToken = accessToken.authenticationToken
-//                    
-//                    let defaults = UserDefaults.standard
-//                    defaults.set(self.fbAccessToken, forKey: "FB_TOKEN")
-//                    
-//                    Tokens.sharedInstance.Token = self.fbAccessToken
-                    
-                    self.getFBUserDetails()
-                }
-                break
-            }
-        }
- */
-
     }
-    
-    func getFBUserDetails() {
-        let request = GraphRequest(graphPath: "me", parameters: ["fields":"email,name"], accessToken: AccessToken.current, httpMethod: .GET, apiVersion: FacebookCore.GraphAPIVersion.defaultVersion)
-        request.start { (response, result) in
-            switch result {
-            case .success(let value):
-                print("success")
-                break
-            case .failed(let error):
-                print(error)
-                DispatchQueue.main.async {
-//                    self.activityIndicator.stopAnimating()
-                    print("failed")
-                    self.facebookButton.isEnabled = true
-                }
-                
-            }
-        }
-    }
-
-
 }
 
